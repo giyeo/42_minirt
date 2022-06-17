cbd46bef8310d333c8f7ec78bcb1a53c223fcdd9
Rafael Paulino <42rpaulino@gmail.com>
2022-06-17 07:36:06 -0300

    removing some boneplate in main

--- src/start_main/main.c
+++ src/start_main/main.c
@@ -24,55 +24,30 @@ void	init_data(t_data *data)
 	data->color = NULL;
 }
 
+char *raw_data[] =
+{"A 0.2 255,255,255"
+,"C -50,0,20 0,0,0 70"
+,"L -40,0,30 0.7 255,255,255"
+,"pl 0,0,0 0,1.0,0 255,0,225"
+,"sp 0,0,20 20 255,0,0"
+,"cy 50.0,0.0,20.6 0,0,1.0 14.2 21.42 10,0,255"};
+
 int main(void)
 {
-	char *A = "A 0.2 255,255,255";
-	char *C ="C -50,0,20 0,0,0 70";
-	char *L ="L -40,0,30 0.7 255,255,255";
-	char *pl ="pl 0,0,0 0,1.0,0 255,0,225";
-	char *sp ="sp 0,0,20 20 255,0,0";
-	char *cy ="cy 50.0,0.0,20.6 0,0,1.0 14.2 21.42 10,0,255";
+	t_data *arguments;
+	int raw_data_size;
+	int i;
 
+	raw_data_size = sizeof(raw_data) / 8;
+	arguments = (t_data *)malloc(sizeof(t_data) * raw_data_size);
+	i = 0;
+	while(i < raw_data_size)
+	{
+		init_data(&arguments[i]);
+		identify_type(raw_data[i], &arguments[i]);
+		print_data(&arguments[i]);
+		i++;
+	}
 
-	// int *list = (int *)parse_three_numbers("134,145,155", 0);
-	// double *dob = (double *)parse_three_numbers("1.1,13.4,14", 1);
-	// int *integer = (int *)parse_one_number("3", 0);
-	// double *doub = (double *)parse_one_number("34.2", 1);
-
-	t_data ambient;
-	t_data camera;
-	t_data light;
-	t_data plane;
-	t_data sphere;
-	t_data cylinder;
-	init_data(&camera);
-	init_data(&light);
-	init_data(&ambient);
-	init_data(&plane);
-	init_data(&sphere);
-	init_data(&cylinder);
-
-	// printf("%d %d %d\n", list[0], list[1], list[2]);
-	// printf("%f %f %f\n", dob[0], dob[1], dob[2]);
-	// printf("%d\n", integer[0]);
-	// printf("%f\n", doub[0]);
-	identify_type(A, &ambient);
-	print_data(&ambient);
-	identify_type(C, &camera);
-	print_data(&camera);
-	identify_type(L, &light);
-	print_data(&light);
-	identify_type(pl, &plane);
-	print_data(&plane);
-	identify_type(sp, &sphere);
-	print_data(&sphere);
-	identify_type(cy, &cylinder);
-	print_data(&cylinder);
-	//printf("%ld\n", data.ambient->color.x);
-
-	// free(integer);
-	// free(doub);
-	// free(list);
-	// free(dob);
 	return(0);
 }
